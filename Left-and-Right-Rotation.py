8ed96136c6066ddb7b54e6a865c683c70711aa2f
manishsharma84 <sharma1402m@gmail.com>
2023-11-24 13:02:18 +0530

    Code changed fully

--- Left-and-Right-Rotation.py
+++ Left-and-Right-Rotation.py
@@ -1,32 +1,26 @@
+# Rotate an Array one by one (left shift)
+def rotatearray(arr, length, shift):
+    itr = 1
+    while itr <= shift:
+        # for left shift
+        first = arr[0]
+        for i in range(length-1):
+            arr[i] = arr[i+1]
+        arr[length-1] = first
+        itr += 1
+
+        # # for Right shift
+        # last = arr[length - 1]
+        # for i in range(length-1, 0, -1):
+        #     arr[i] = arr[i-1]
+        # arr[0] = last
+        # itr += 1
+    return arr
+
+
 A = [1, 2, 3, 4, 5, 6]
-shift = 2
-def rotationByLeft(Arr_num, sift):
-    for i in range(0, sift):
-        first= Arr_num[0]
-        for i in range(0, len(Arr_num)-1):
-            Arr_num[i] = Arr_num[i+1]
-        Arr_num[len(Arr_num)-1]= first
-    return Arr_num
-def rotationByRight(Arr_num, sift):
-    for i in range(0, sift):
-        last = Arr_num[len(Arr_num)-1]
-        for j in range(len(Arr_num)-1, 0, -1):
-            Arr_num[j] = Arr_num[j-1]
-        Arr_num[0] = last
-    return Arr_num
+n = len(A)
+s = 4
 
-def printArray(Arr):
-    for i in range(0, len(Arr)):
-        print(Arr[i], end=' ')
-
-print("\nArray Rotation by Right")
-print("Array", A)
-print("Rotated")
-RotatedArrayByRight = rotationByRight(A, shift)
-printArray(RotatedArrayByRight)
-
-print("\n\nArray Rotation by Left")
-print("Array", A)
-print("Rotated")
-RotatedArrayByLeft = rotationByLeft(A, shift)
-printArray(RotatedArrayByLeft)
+rotated = rotatearray(A, n, s)
+print(rotated)
